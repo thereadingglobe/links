09a3817afced0e229beef5f93e107369173bf4dd
thereadingglobe <145748835+thereadingglobe@users.noreply.github.com>
2023-09-23 11:19:11 +0700

    Update search.js

--- assets/search.js
+++ assets/search.js
@@ -3,22 +3,21 @@ layout: null
 permalink: /search.json
 ---
 
-var pages = [
-  {% for page in site.pages %}
+var links = [
+  {% for link in site.links %}
     {
-      "title": "{{ page.title | escape }}",
-      "description": "{{ page.description | escape }}",
-      "url": "{{ page.url | absolute_url }}"
+      "name": "{{ link.Name | escape }}",
+      "url": "{{ link.URL | absolute_url }}"
     }{% unless forloop.last %},{% endunless %}
   {% endfor %}
 ];
 
 var fuseOptions = {
-  keys: ["title", "description"],
+  keys: ["name"],
   threshold: 0.3
 };
 
-var fuse = new Fuse(pages, fuseOptions);
+var fuse = new Fuse(links, fuseOptions);
 
 var searchInput = document.getElementById("search-input");
 
@@ -31,8 +30,7 @@ searchInput.addEventListener("input", function () {
 
   for (var i = 0; i < results.length; i++) {
     html += '<div class="search-result">';
-    html += '<h2><a href="' + results[i].url + '">' + results[i].title + '</a></h2>';
-    html += '<p>' + results[i].description + '</p>';
+    html += '<h2><a href="' + results[i].url + '">' + results[i].name + '</a></h2>';
     html += '</div>';
   }
 
